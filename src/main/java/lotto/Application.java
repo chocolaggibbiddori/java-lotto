671f32ecfda35de7c887d92f573a0adf1937a4ca
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-08-22 17:17:19 +0900

    Feat : 구입 금액을 입력 받는 기능 추가
    구입 금액을 입력 받아 올바른 값인지 검사한 후에 money 변수에 저장한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 1. 구입 금액을 입력 받는다.
+        System.out.println("구매 금액을 입력해 주세요.");
+        String inputMoney = Console.readLine();
+        int money = Validator.checkInputMoney(inputMoney);
     }
 }
