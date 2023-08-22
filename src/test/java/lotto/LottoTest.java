0cc05f0e19c685cd1832da78cc78a814b50af5a8
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-08-22 18:57:16 +0900

    Test : 로또 발행 기능
    로또가 정상적으로 출력되는지, 로또 발행 개수가 알맞는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -18,10 +18,23 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("주어진 금액에 맞는 로또 리스트를 발행한다.")
+    @Test
+    void createLotto() {
+        //g
+        int money = 8000;
+        LottoPublish lottoPublish = new AutoLottoPublisher();
+
+        //w
+        List<Lotto> lottoList = lottoPublish.createLottoList(money);
+
+        //t
+        System.out.println(lottoList);
+        assertThat(lottoList.size()).isEqualTo(8);
+    }
 }
