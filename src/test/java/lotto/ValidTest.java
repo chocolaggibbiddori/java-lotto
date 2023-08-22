25778f0a19990c7e339897a4f680b45c74ab4a73
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-08-22 19:53:46 +0900

    Test : 당첨 번호를 입력 받을 때 규격을 확인
    refactor : 같은 메서드를 테스트 하는데 파라미터만 다른 경우, @ValueSource, @ParameterizedTest를 이용해 중복을 제거함

--- src/test/java/lotto/ValidTest.java
+++ src/test/java/lotto/ValidTest.java
@@ -2,6 +2,10 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -20,23 +24,33 @@ public class ValidTest {
         assertThat(checkedMoney).isEqualTo(8000);
     }
 
-    @DisplayName("구입 금액이 숫자가 아니라면 예외가 발생한다.")
-    @Test
-    void inputPurchaseAmountTest_fail_NumberFormat() {
-        //g
-        String inputMoney = "1000abc";
-
+    @DisplayName("구입 금액이 숫자가 아니거나 1,000원 단위가 아니라면 예외가 발생한다.")
+    @ValueSource(strings = {"1000abc", "35233"})
+    @ParameterizedTest
+    void inputPurchaseAmountTest_fail_NumberFormat(String inputMoney) {
         //t
         assertThatThrownBy(() -> Validator.checkInputMoney(inputMoney)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구입 금액이 1,000원 단위가 아니라면 예외가 발생한다.")
+    @DisplayName("당첨 번호는 규격에 맞아야 한다.")
     @Test
-    void inputPurchaseAmountTest_fail_amount() {
+    void inputWinningNumbers_Success() {
         //g
-        String inputMoney = "35233";
+        String inputWinningNumbers = "1, 3, 5, 7, 9, 11";
+
+        //w
+        List<Integer> numbers = Validator.checkInputWinningNumbers(inputWinningNumbers);
 
         //t
-        assertThatThrownBy(() -> Validator.checkInputMoney(inputMoney)).isInstanceOf(IllegalArgumentException.class);
+        System.out.println("numbers = " + numbers);
+        assertThat(numbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("당첨 번호가 규격에 맞지 않으면 예외가 발생한다.")
+    @ValueSource(strings = {"[1, 2, 3, 4, 5, 6,]", "0,1,3,7,50,70", "1,30, 45"})
+    @ParameterizedTest
+    void inputWinningNumbers_FailByGrammar(String inputWinningNumbers) {
+        //t
+        assertThatThrownBy(() -> Validator.checkInputWinningNumbers(inputWinningNumbers)).isInstanceOf(IllegalArgumentException.class);
     }
 }
